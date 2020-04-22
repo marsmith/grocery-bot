ec95fe19742d31cd5c3b70e35e2867a463f01c39
Marty <marsmith@usgs.gov>
2020-04-22 14:59:51 -0400

    init

---
+++ config.py
@@ -0,0 +1,29 @@
+#configure stores here
+
+stores = [
+    {
+    "name": "albany ghetto chopper",
+    "type": "market-32",
+    "baseUrl": "www.instacart.com",
+    "sessionCookie": "_instacart_session=VTFFZFB5eHVrZkxkSXlPWGpYOHZUY1N4ZjhpSlFkb3pNT2tEb0Ewa1Z0RjJiRmxSRlhmK2ZuY3lMQXJoK3NlOE16eWxpR3JWd1oxL1E5UFhYd1JJcEc3cFB2UDN1M0dLdC9IRkpicTZaaDU2T2t3QVNlVnR2SGFucEdjdnVKeHB0UEdwTWpnVlduTE56dzMxdDg0amlnN3A0OHNxUGZ2c0hFelVuRVBiM0RUR3ZLMmV0cVpEcTljaFhMYmhMS0hiWUtIQWpXNUJEOXBsdHo3amhlNFNhSnFEdnI0TUhRZnVnanZOUmIvSUJVOGVJZzFHNFdhSVdJYlVYTTlIWDk5T1B0M3U1WW9ZVURMRkY2cllqREFLV0Ztcnk3SFFtRysxVVdteEpudlUrdjF6Z3liVE1sdS9lRTJiNVJwQndURHpHdktIVFdvdHBqMFFySTQ2cEdRQ1lYU2JhdUl0YkFtb2dBWGoxTnZNZlc0ZUovK2JwN2lRUFRRczlaRkd6RUxSQ1R6ZEFpQUhrM2kveXViT3MrTllGQT09LS1QZ2g3TzhKVmtEMkdaMkIzZjY5TS9BPT0%3D--dce793d1788ff77b22e99cda632a116f74bca18e; Path=/; Domain=.instacart.com; Secure; HttpOnly;"
+    },
+    # {
+    # "name": "EG Market 32",
+    # "type": "market-32",
+    # "baseUrl": "www.instacart.com",
+    # "sessionCookie": "_instacart_session=N0NzMVRvZVBYSUpZUHRka1dhMFBBdzVISGJvbTYxVThrUlRwaWlJeWdNbDFIb2Y3RHd0SlZiWG9HZnVWd1hZVHlLbkxBTVlBK2h3UXd5S0RhVmd5cFIrZXNnTVF0a2Z6ZzNDalRjcXYyb3JPcVRKUGJoVFdHUGtTV1J1U2RaMnFQQ0VScmwrMmc4RGRhRTMvL2ZFa3pkNWs2VWVoMXRIRGFId0pHdnkvWjcxREdxa1pxWTV5ajhHTTB0aklDUnJWY0U5d0ZFN2NiUHVnWkhWUldwaEZPdWxZNmpjQnM0bGgwVnVTRXdXRHpGamphSVRNalBYbVBzWnQwWDhvQkMySjd3N1h6SVJpbWpSU3ZnNy81T0JiUFgwMUIxWWpGWVc3MGJkdTI1dk9NczRBaGdVcHdKQ056ei83cVVqbzlnR2Vzb3dROUMrSHNmL3J3RndtOFNCSTVRPT0tLWM2d255MExHNUxZZ3VVWUNjUml1bWc9PQ%3D%3D--39d1ce09988e6837ffef7f526723fa27a5c12a2c; Path=/; Domain=.instacart.com; Secure; HttpOnly;"
+    # },
+    # {
+    # "name": "East Greenbush Hannaford",
+    # "type": "hannaford",
+    # "baseUrl": "www.hannaford.com",
+    # "sessionCookie": "PIPELINE_SESSION_ID=bc5b137e62784c76aabdd601d8473bc4;"
+
+    # },
+    # {
+    # "name": "Wynantskill Hannaford",
+    # "type": "hannaford",
+    # "baseUrl": "www.hannaford.com",
+    # "sessionCookie": "PIPELINE_SESSION_ID=829efca7b6024bb1b46cb1aa1679e010;"
+    # }
+]
---
+++ instacart-delivery-time-alert.py
@@ -0,0 +1,179 @@
+import http.client
+import pprint
+import json
+from datetime import datetime
+import os
+import sys
+import config
+
+def sendEmail(text):
+    #https://www.geeksforgeeks.org/send-mail-attachment-gmail-account-using-python/
+
+    # Python code to illustrate Sending mail with attachments 
+    # from your Gmail account  
+
+    # libraries to be imported 
+    import smtplib 
+    from email.mime.multipart import MIMEMultipart 
+    from email.mime.text import MIMEText 
+    from email.mime.base import MIMEBase 
+    from email import encoders
+    import secrets
+
+    fromaddr = "martynjsmith@gmail.com"
+    toaddr = "martynjsmith@gmail.com"
+
+    # instance of MIMEMultipart 
+    msg = MIMEMultipart() 
+
+    # storing the senders email address   
+    msg['From'] = fromaddr 
+
+    # storing the receivers email address  
+    msg['To'] = toaddr 
+
+    # storing the subject  
+    msg['Subject'] = "Grocery Pickup Slot Found"
+
+    # string to store the body of the mail 
+    body = text
+
+    # attach the body with the msg instance 
+    msg.attach(MIMEText(body, 'plain')) 
+
+    # creates SMTP session 
+    s = smtplib.SMTP('smtp.gmail.com', 587) 
+
+    # start TLS for security 
+    s.starttls() 
+
+    # Authentication 
+    s.login(fromaddr, secrets.password) 
+
+    # Converts the Multipart msg into a string 
+    text = msg.as_string() 
+
+    # sending the mail 
+    s.sendmail(fromaddr, toaddr, text) 
+
+    # terminating the session 
+    s.quit() 
+
+def send_results(date, store, details, url):
+    message = "On {}, found available pickup slots at {}: \r\nReserve now at: {}".format(date, store, url)
+    for slot in details:
+        message += "\r\nSlot: " + slot
+    sendEmail(message)
+    print(message)
+
+def main():
+    print(f"[{datetime.now()}]: --- Checking for Instacart Pickup Time Availability --") 
+
+    for item in config.stores:
+
+        store = item["type"]
+        name = item["name"]
+        baseUrl = item["baseUrl"]
+        sessionCookie = item["sessionCookie"]
+
+        conn = http.client.HTTPSConnection(baseUrl)
+        payload = ''
+
+
+        if store == "market-32":
+            storeUrl = "https://www.instacart.com/store/" + store + "/storefront"
+            reqUrl = f"/v3/containers/{store}/next_gen/retailer_information/content/pickup?source=web"
+
+            headers = {
+                'Referer': 'https://www.hannaford.com/checkout/retrieve_pickup_timeslots.cmd',
+                'Cookie': sessionCookie
+            }
+
+            conn.request("GET", reqUrl, payload, headers)
+            res = conn.getresponse()
+            if (res.status == 200):
+                data = res.read()
+                json_data = json.loads(data.decode("utf-8"))
+
+                api_result = json_data["container"]["modules"][0]["types"][0]
+
+                # Two possible values for api_result = icon_info or error
+                # 'error' means no Pickup times are available!
+                # 'icon_info' means Pickup options are listed into second list available in "modules" section
+                if api_result == 'error':
+                    # Do Nothing!
+                    print(pprint.pformat(json_data["container"]["modules"][0]['data']['title']))
+                    print("No Pickup times are available! Let's check again in 5 minutes!")
+
+                if api_result == 'icon_info':
+                    print('Pickup Time Windows available, Send Alert!')
+                    # We are in luck! Send Alert to Needy folks so that they can place their order right away!
+                    Pickup_days_json = json_data["container"]["modules"][1]['data']['service_options']['service_options']['days']
+
+                    Pickup_window_details = []
+
+                    #print(Pickup_days_json)
+                    for each_Pickup_day in Pickup_days_json:
+
+                        for option in each_Pickup_day["options"]:
+
+                            Pickup_window_details.append(option["full_window"])
+                    
+                    print(Pickup_window_details)
+
+                    send_results(datetime.now(), name, Pickup_window_details, storeUrl)
+
+            else:
+                print(f'Error Code: {res.status}')
+
+
+        print("-- End of Checking on Instacart Pickup Time Availability --") 
+
+     
+        if store == "hannaford":
+            storeUrl = "http://" + baseUrl
+            reqUrl = "/checkout/retrieve_pickup_timeslots.cmd"
+
+            headers = {
+                'Referer': 'https://www.hannaford.com/checkout/retrieve_pickup_timeslots.cmd',
+                'Cookie': sessionCookie
+            }
+
+            conn.request("GET", reqUrl, payload, headers)
+            res = conn.getresponse()
+            if (res.status == 200):
+                data = res.read()
+                json_data = json.loads(data.decode("utf-8"))
+
+                Pickup_window_details = []
+
+                for day in json_data["days"]:
+                    #print(day)
+                    date = day["date"]
+
+                    print('date:', date)
+
+                    if len(day["pickupTimes"]) > 0:
+
+                        for pickupTime in day["pickupTimes"]:
+
+                            Pickup_window_details.append(date + " " + pickupTime["timeSlot"])
+
+                            if pickupTime["shortMessage"] != 'Unavailable':
+                                print('Found a slot:', pickupTime)
+                                Pickup_window_details.append(date + " " + pickupTime["timeSlot"])
+
+                if len(Pickup_window_details) == 0:
+                    print("No Pickup times are available! Let's check again in 10 minutes!")
+                else:
+                    print('Pickup Time Windows available, Send Alert!')
+                    send_results(datetime.now(), name, Pickup_window_details, storeUrl)
+            else:
+                print(f'Error Code: {res.status}')
+            
+
+
+# Reference Implementation: https://github.com/utkuufuk/ping-sm/blob/master/__main__.py
+# https://utkuufuk.com/2020/03/28/grocery-scraping/
+if __name__ == '__main__':
+    main()
