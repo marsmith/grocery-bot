e7a7c28801c3a5f4b21af57b592200be385124ef
Marty <marsmith@usgs.gov>
2020-04-27 17:26:52 -0400

    remove cjs

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ stores = [
     # "baseUrl": "www.instacart.com",
     # "sessionCookie": "_instacart_session=VTFFZFB5eHVrZkxkSXlPWGpYOHZUY1N4ZjhpSlFkb3pNT2tEb0Ewa1Z0RjJiRmxSRlhmK2ZuY3lMQXJoK3NlOE16eWxpR3JWd1oxL1E5UFhYd1JJcEc3cFB2UDN1M0dLdC9IRkpicTZaaDU2T2t3QVNlVnR2SGFucEdjdnVKeHB0UEdwTWpnVlduTE56dzMxdDg0amlnN3A0OHNxUGZ2c0hFelVuRVBiM0RUR3ZLMmV0cVpEcTljaFhMYmhMS0hiWUtIQWpXNUJEOXBsdHo3amhlNFNhSnFEdnI0TUhRZnVnanZOUmIvSUJVOGVJZzFHNFdhSVdJYlVYTTlIWDk5T1B0M3U1WW9ZVURMRkY2cllqREFLV0Ztcnk3SFFtRysxVVdteEpudlUrdjF6Z3liVE1sdS9lRTJiNVJwQndURHpHdktIVFdvdHBqMFFySTQ2cEdRQ1lYU2JhdUl0YkFtb2dBWGoxTnZNZlc0ZUovK2JwN2lRUFRRczlaRkd6RUxSQ1R6ZEFpQUhrM2kveXViT3MrTllGQT09LS1QZ2g3TzhKVmtEMkdaMkIzZjY5TS9BPT0%3D--dce793d1788ff77b22e99cda632a116f74bca18e; Path=/; Domain=.instacart.com; Secure; HttpOnly;"
     # },
-    {
-    "name": "Slingerlands Price Chopper",
-    "emailList" : ["cjsmufc1@gmail.com", "martynjsmith@gmail.com"],
-    "type": "price-chopper-ny",
-    "baseUrl": "www.instacart.com",
-    "sessionCookie": "_instacart_session=ZlBkRi9oVEE2b2N5TW1XeW45TXVpcW9SOG1mWHZtRDMxUlc5SStXaU5EeFRpN3pXcm1TUG0xMXJ1ODBiaUlTbThjcFV2dUZYUGNyZkJmOXdzcUx1RkJhalhwVWZUd2N2ejNPQUYrTjg0dzdRWTR5REN6dVROc2FFS2liZUVtOEhkR1cyVlR6djJmWkJWaUdtcXZqMWRNMUpGUm5qSUFQRlNaRFptRG44M3p4YWpDMFJ0U3phMUdhR1dRaURhaTYzY0JhU0RkWExIMkVCSzI5VnVINi9mSG9jWS82bnVBeUk0dFJBODh2eTlpODdmZ0NvMGpPaTdzZUFiOC9tNVUxUjQvaWNoWUF4b0RCNml1YVhyRGVqYU1OSGFqbndvSmJPZTcwYk4rZGFnamEwL1NGZHpHRnhvTE9YM096VWsyQlBVQVFTdmlSQWdCQ1hXNmVMWFBreUpiN3lJL0Q1cThLa08wZkdNcXdNaDl2MHlzWHpENnk5c3BzTCsybE5mbHRQU0ZCNHEzQ0wxZXBCWEtobElYSVVFUT09LS1SbE1QMkhZamwyMVZpZHJmK1JnaXp3PT0%3D--cf2368aed6c253290cde63e781184f8fa05fb565; Path=/; Domain=.instacart.com; Secure; HttpOnly;"
-    },
+    # {
+    # "name": "Slingerlands Price Chopper",
+    # "emailList" : ["cjsmufc1@gmail.com", "martynjsmith@gmail.com"],
+    # "type": "price-chopper-ny",
+    # "baseUrl": "www.instacart.com",
+    # "sessionCookie": "_instacart_session=ZlBkRi9oVEE2b2N5TW1XeW45TXVpcW9SOG1mWHZtRDMxUlc5SStXaU5EeFRpN3pXcm1TUG0xMXJ1ODBiaUlTbThjcFV2dUZYUGNyZkJmOXdzcUx1RkJhalhwVWZUd2N2ejNPQUYrTjg0dzdRWTR5REN6dVROc2FFS2liZUVtOEhkR1cyVlR6djJmWkJWaUdtcXZqMWRNMUpGUm5qSUFQRlNaRFptRG44M3p4YWpDMFJ0U3phMUdhR1dRaURhaTYzY0JhU0RkWExIMkVCSzI5VnVINi9mSG9jWS82bnVBeUk0dFJBODh2eTlpODdmZ0NvMGpPaTdzZUFiOC9tNVUxUjQvaWNoWUF4b0RCNml1YVhyRGVqYU1OSGFqbndvSmJPZTcwYk4rZGFnamEwL1NGZHpHRnhvTE9YM096VWsyQlBVQVFTdmlSQWdCQ1hXNmVMWFBreUpiN3lJL0Q1cThLa08wZkdNcXdNaDl2MHlzWHpENnk5c3BzTCsybE5mbHRQU0ZCNHEzQ0wxZXBCWEtobElYSVVFUT09LS1SbE1QMkhZamwyMVZpZHJmK1JnaXp3PT0%3D--cf2368aed6c253290cde63e781184f8fa05fb565; Path=/; Domain=.instacart.com; Secure; HttpOnly;"
+    # },
     {
     "name": "East Greenbush Market 32",
     "emailList" : ["martynjsmith@gmail.com", "holligan.joseph@gmail.com"],
